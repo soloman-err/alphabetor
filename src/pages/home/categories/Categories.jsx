f5fe3d98ad18b90012a4d153a96cebc4dee7c7d5
solo <soloman.err@.com>
2023-09-17 23:02:52 +0600

    Layout: category section layout updated

--- src/pages/home/categories/Categories.jsx
+++ src/pages/home/categories/Categories.jsx
@@ -1,10 +1,48 @@
 import Title from '../../../components/title/Title';
 
 const Categories = () => {
+  const categories = [
+    'Mathematics',
+    'Science',
+    'History',
+    'Literature',
+    'Computer Science',
+    'Geography',
+    'Physics',
+    'Chemistry',
+    'Biology',
+    'Economics',
+    'Philosophy',
+    'Psychology',
+    'Music',
+    'Art',
+    'Health and Wellness',
+    'Language and Linguistics',
+    'Business and Finance',
+    'Engineering',
+    'Political Science',
+    'Environmental Science',
+  ];
+
   return (
     <section className="alpha-container mt-20">
+      {/* SECTION TITLE */}
       <div>
         <Title title={'Alpha Category'} />
+      </div>
+
+      {/* CATEGORIES/ROUTES */}
+      <div className="mt-5">
+        <ul className="flex flex-wrap gap-3">
+          {categories.map((category, index) => (
+            <li
+              key={index}
+              className="font-normal py-1 px-5 bg-cyan-500/10 rounded-sm"
+            >
+              <button className='cursor-pointer'>{category}</button>
+            </li>
+          ))}
+        </ul>
       </div>
     </section>
   );
