d9ecd9fb6dae185414be4062be4d56e403735aef
solo <soloman.err@.com>
2023-10-19 21:36:25 +0600

    dashboard layout updated

--- src/layout/Dashboard.jsx
+++ src/layout/Dashboard.jsx
@@ -16,9 +16,8 @@ import { Link, Outlet } from 'react-router-dom';
 
 const menuItems = [
   { id: 1, icon: FaHome, text: 'Home', to: '/' },
-  { id: 2, icon: FaChartBar, text: 'Analytics', to: 'analytics' },
-  { id: 3, icon: FaFileAlt, text: 'Reports', to: 'reports' },
-  { id: 4, icon: FaBook, text: 'Books', to: 'all-books' },
+  { id: 2, icon: FaChartBar, text: 'Dashboard', to: '/dashboard' },
+  { id: 3, icon: FaBook, text: 'Books', to: 'all-books' },
   { id: 4, icon: FaAddressBook, text: 'Add New Book', to: 'add-new-book' },
   {
     id: 5,
@@ -32,8 +31,9 @@ const menuItems = [
     text: 'Total Students',
     to: 'total-students',
   },
-  { id: 7, icon: FaCog, text: 'Settings', to: 'settings' },
-  { id: 8, icon: FaRegUser, text: 'Logout' },
+  { id: 7, icon: FaFileAlt, text: 'Reports', to: 'reports' },
+  { id: 8, icon: FaCog, text: 'Settings', to: 'settings' },
+  { id: 9, icon: FaRegUser, text: 'Logout' },
 ];
 
 const Dashboard = () => {
@@ -45,25 +45,26 @@ const Dashboard = () => {
   };
 
   return (
-    <div className="flex flex-row bg-gray-100 min-h-screen">
+    <div className="flex flex-row min-h-screen">
       {/* Sidebar Navigation Drawer */}
-      <aside className="bg-[#113C4B] z-50 text-white space-y-2 py-2 transform transition-transform duration-500">
-        <div className="relative flex items-center justify-between gap-2 px-5">
+      <aside className="bg-[#113C4B] z-50 text-white space-y-2 py-2">
+        <div className="flex items-center justify-between gap-2">
           <div
-            className={`text-xl font-semibold uppercase ${
-              isDrawerOpen ? '' : 'hidden py-4'
+            className={`text-xl font-bold uppercase ${
+              isDrawerOpen ? 'px-3' : 'hidden py-4'
             }`}
           >
-            Dashboard
+            <Link to={'/dashboard'}>Dashboard</Link>
           </div>
+
           <button
             onClick={toggleDrawer}
-            className="border border-white rounded-sm bg-[#113C4B] text-white focus:outline-none"
+            className="border border-white rounded-sm bg-[#113C4B] text-white focus:outline-none my-1 mx-3"
           >
             {isDrawerOpen ? (
-              <FaAngleLeft className="text-2xl" />
+              <FaAngleLeft className="text-xl" />
             ) : (
-              <FaAngleRight className="text-2xl" />
+              <FaAngleRight className="text-xl" />
             )}
           </button>
         </div>
@@ -74,21 +75,21 @@ const Dashboard = () => {
           <ul className="flex flex-col h-full space-y-1">
             {menuItems?.map((item) => (
               <li
+                onClick={() => setIsDrawerOpen(false)}
                 key={item.id}
-                className={`hover:bg-gray-200 hover:text-black font-semibold gap-2 ${
-                  item.id === 6 && 7 ? 'mt-5' : ''
+                className={`hover:text-[#113C4B] hover:bg-gray-50 font-semibold hover:font-bold gap-2 ${
+                  item.id === 6 && 7 ? 'mt-' : ''
                 }`}
               >
                 {isDrawerClosed && (
                   <item.icon
                     onClick={setIsDrawerClosed(!isDrawerClosed)}
-                    className="mr-2"
+                    className="mr-"
                   />
                 )}
-
                 <Link
                   to={item.to}
-                  className="flex flex-row py-2 gap-2 w-full px-5"
+                  className="flex flex-row py-2 px-3 gap-2 w-full"
                 >
                   {<item.icon size={20} />}
                   {isDrawerOpen && item.text}
@@ -100,7 +101,7 @@ const Dashboard = () => {
       </aside>
 
       {/* Main Content Area */}
-      <div className="p-2 w-full h-full">
+      <div className="w-full">
         <Outlet />
       </div>
     </div>
--- src/pages/dashboard/DashboardHome.jsx
+++ src/pages/dashboard/DashboardHome.jsx
@@ -1,7 +1,144 @@
 import React from 'react';
+import { FaArrowUp, FaBell, FaCog, FaFilter, FaUser } from 'react-icons/fa';
+import { NavLink } from 'react-router-dom';
+import Title from '../../components/title/Title';
+import Chart from './admin/chart/Chart';
+
+export const UsersInfo = () => {
+  return (
+    <div className="flex flex-row items-center gap-2">
+      <div className="flex gap-2">
+        <FaBell size={30} className="bg-gray-300 p-2 rounded-sm" />
+        <FaCog size={30} className="bg-gray-300 p-2 rounded-sm" />
+      </div>
+      <div className="flex items-center gap-2">
+        <FaUser size={30} className="bg-gray-300 p-2 rounded-full" />
+        {/* <div className="flex flex-col text-start ">
+          <h2 className="font-bold text-sm">Super Admin</h2>
+          <span className="text-xs font-semibold text-gray-600">
+            Super Admin
+          </span>
+        </div> */}
+      </div>
+    </div>
+  );
+};
 
 const DashboardHome = () => {
-  return <div>Alphabetor Dashboard</div>;
+  return (
+    <div className="p-2">
+      {/* Dashboard Header */}
+      <div className="flex flex-col md:flex-row justify-between w-full">
+        <div className="flex flex-row justify-between items-center mb-2 md:mb-0">
+          <div>
+            <Title title={'Dashboard'} />
+          </div>
+
+          <div className="md:hidden">
+            <UsersInfo />
+          </div>
+        </div>
+
+        {/* Search, Settings & User Info */}
+        <div className="hidden md:flex gap-4 items-center">
+          <div className="">
+            <input
+              type="text"
+              className="rounded-full text-sm py-1 px-2 bg-gray-200 w-full focus:outline-none focus:border focus:border-gray-400"
+              placeholder="Search.."
+            />
+          </div>
+
+          <UsersInfo />
+        </div>
+      </div>
+
+      <hr className="my-2" />
+
+      {/* Dashboard Analytics */}
+      <div className="flex flex-col md:flex-row gap-2 w-full">
+        {/* Chart & Revenues */}
+        <section className="md:w-4/6 p-2 h-full bg-gray-200 rounded-md">
+          {/* Navigation */}
+          <nav className="">
+            <ul className="flex flex-wrap gap-4 text-sm font-semibold text-black/80 border-b-2 w-fit border-black/60 pb-2">
+              <li>
+                <NavLink to={''}>Overview</NavLink>
+              </li>
+              <li>
+                <NavLink to={''}>Unpaid Students</NavLink>
+              </li>
+              <li>
+                <NavLink to={''}>School Expense</NavLink>
+              </li>
+              <li>
+                <NavLink to={''}>Recent Students</NavLink>
+              </li>
+              <li>
+                <NavLink to={''}>Courses</NavLink>
+              </li>
+            </ul>
+          </nav>
+
+          {/* Earnings & Expenses Chart */}
+          <div className="flex flex-col h-full justify-between">
+            <div className="flex flex-col md:flex-row justify-betwee py-2 gap-2">
+              {/* Earnings */}
+              <div className="bg-gray-300 p-2 rounded-md text-sm space-y-4">
+                <h4>Total Earnings</h4>
+
+                <div className="flex justify-between gap-2">
+                  <div>
+                    <h2 className="font-bold text-xl">$58,123.00</h2>
+                    <span>This month</span>
+                  </div>
+
+                  <span className="flex items-center text-xs bg-white rounded-full font-semibold mt-auto h-full w-fit p-1">
+                    <FaArrowUp className="text-green-600" />
+                    +50.03%
+                  </span>
+                </div>
+              </div>
+
+              {/* Expenses */}
+              <div className="bg-gray-300 p-2 rounded-md text-sm space-y-4">
+                <h4>Total Expenses</h4>
+
+                <div className="flex justify-between gap-2">
+                  <div>
+                    <h2 className="font-bold text-xl">$18,203.00</h2>
+                    <span>This month</span>
+                  </div>
+
+                  <span className="flex items-center text-xs bg-white rounded-full font-semibold mt-auto h-full w-fit p-1">
+                    <FaArrowUp className="text-green-600" />
+                    +50.03%
+                  </span>
+                </div>
+              </div>
+            </div>
+
+            {/* Chart */}
+            <div className="w-full h-80 text-xs font-semibold bg-gray-300 p-2 rounded-md">
+              <Chart />
+            </div>
+          </div>
+        </section>
+
+        {/* Courses Activities */}
+        <aside className="md:w-2/6 p-2 h-full bg-gray-200 rounded-md">
+          <div>
+            <div className="flex items-center justify-between">
+              <h2 className="text-sm font-bold">Student Activity</h2>
+              <FaFilter size={14} />
+            </div>
+          </div>
+
+          <hr className="my-2" />
+        </aside>
+      </div>
+    </div>
+  );
 };
 
 export default DashboardHome;
