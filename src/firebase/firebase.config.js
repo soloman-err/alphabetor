24ba96e6a7f22778444c0f180f4c463999957130
solo <soloman.err@.com>
2023-10-26 20:03:27 +0600

    generic form integrated for users

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,9 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+// Firebase configuration
 const firebaseConfig = {
   apiKey: 'AIzaSyAUKb5xGPovDtnp1ERnn4fvR7OmFM0eynQ',
   authDomain: 'alphabetor-edu.firebaseapp.com',
@@ -13,5 +10,4 @@ const firebaseConfig = {
   appId: '1:806415210695:web:786ca870f534d12b4e46e2',
 };
 
-// Initialize Firebase
 export const app = initializeApp(firebaseConfig);
